f82c31cb205a869b30c84f80a8aa3805e6d3cc24
MunguiaGarciaMishel <89650954+MunguiaGarciaMishel@users.noreply.github.com>
2021-11-09 18:55:10 -0600

    Tercera versión

---
+++ app/src/main/java/com/mishel/munguia/garcia/logica/Calculadora.java
@@ -0,0 +1,21 @@
+package com.mishel.munguia.garcia.logica;
+
+import java.io.Serializable;
+
+public class Calculadora implements Serializable {
+
+    public void suma(){
+
+    }
+    public void resta(){
+
+    }
+    public void multiplicación(){
+
+    }
+    public void división(){
+
+    }
+
+
+}
