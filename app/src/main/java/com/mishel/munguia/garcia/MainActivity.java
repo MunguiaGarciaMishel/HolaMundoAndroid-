38a14915b47e11f1b9de469054e95cbbdc077b97
MunguiaGarciaMishel <89650954+MunguiaGarciaMishel@users.noreply.github.com>
2021-11-26 02:16:23 -0600

    se agrego un metodo para la suma

--- app/src/main/java/com/mishel/munguia/garcia/MainActivity.java
+++ app/src/main/java/com/mishel/munguia/garcia/MainActivity.java
@@ -6,9 +6,33 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void suma(View miView)
+    { }
+    
+    private Button Button_suma;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button_suma = findViewById(R.id.Button_suma);
+
+        Button_suma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirSuma();
+            }
+        });
+
+    }
+
+    private void abrirOperacion() {
+        Intent intent = new Intent(this , operacion.class);
+        startActivity(intent);
+    }
+
+    private void abrirSuma() {
+        Intent intent = new Intent(this , SumaActivity.class);
+        startActivity(intent);
     }
 }
